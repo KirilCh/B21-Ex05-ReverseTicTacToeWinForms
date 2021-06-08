4315bf95802c260813e7cb95aa5abd6a9370fab0
KirilCh <59510467+KirilCh@users.noreply.github.com>
2021-06-08 20:42:21 +0300

    Update

--- B21 Ex05 Kiril 323439711 Yonatan 204307672/BoardForm.cs
+++ B21 Ex05 Kiril 323439711 Yonatan 204307672/BoardForm.cs
@@ -170,6 +170,7 @@ namespace B21_Ex05_Kiril_323439711_Yonatan_204307672
             {
                 //Play again, with the same settings
                 this.Hide();
+                
                 new BoardForm(m_BoardSize, m_LabelPlayer1Name.Text, m_PlayingVersus, m_LabelPlayer2Name.Text).Show();
             }
             else
